545ff613ed0edafc4f1b6a6402e0cb3b0ae0e352
thms200 <thms0504@gmail.com>
2019-11-20 14:03:02 +0900

    Modyfied random user

--- src/App.js
+++ src/App.js
@@ -1,65 +1,14 @@
 import React, { Component } from 'react';
-import Name_after from "./Component/name2.jpg";
-import Location_after from './Component/location2.jpg';
-import Mail_after from './Component/mail2.jpg';
-import Phone_after from './Component/phone2.jpg';
-import Name from './Component/name.jpg';
-import Phone from './Component/phone.jpg';
-import Location from './Component/location.jpg';
-import Mail from './Component/mail.jpg';
 import Header from "./Component/Header";
 import Section from "./Component/Section";
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: ["Brad Gibson", "My name is", Name_after],
-      location: ["9278 new road, kilcoole", "My Address is", Location_after],
-      mail: ["brad.gibson@example.com", "My mail address is", Mail_after],
-      phone: ["011-962-7516", "My phone number is", Phone_after],
-      key: ["Brad Gibson", "My name is"],
-      originalImg: [Name, Phone, Location, Mail]
-    }
-  }
-
-  makeImg(event) {
-    let li = document.querySelectorAll(".li");
-    for(let i = 0; i < li.length; i++) {
-      li[i].src = this.state.originalImg[i];
-    }
-  }
-  
+class App extends Component {  
   render() {
     return (
       <div>
         <Header></Header>
-        <Section
-          data={this.state}
-          selectValue={this.state.key[0]}
-          selectIntro={this.state.key[1]}
-          makeInfo={function(event){
-            if(event.target.alt === "location"){
-              this.makeImg(event)
-              this.setState({key:[this.state.location[0], this.state.location[1]]});
-              event.target.src = this.state.location[2];          
-            } else if(event.target.alt === "name"){
-              this.makeImg(event)
-              this.setState({key:[this.state.name[0], this.state.name[1]]});
-              event.target.src = this.state.name[2];
-            } else if(event.target.alt === "phone"){
-              this.makeImg(event)
-              this.setState({key:[this.state.phone[0], this.state.phone[1]]});
-              event.target.src = this.state.phone[2];
-            } else if(event.target.alt === "mail"){
-              this.makeImg(event)
-              this.setState({key:[this.state.mail[0], this.state.mail[1]]});
-              event.target.src = this.state.mail[2];
-            }
-          }.bind(this)}
-        >
-        </Section>
+        <Section></Section>
       </div>
     )
   }
--- src/Component/Section.js
+++ src/Component/Section.js
@@ -1,23 +1,108 @@
 import React, { Component } from 'react';
+import Name_after from "./name2.jpg";
+import Location_after from './location2.jpg';
+import Mail_after from './mail2.jpg';
+import Phone_after from './phone2.jpg';
+import Name from './name.jpg';
+import Phone from './phone.jpg';
+import Location from './location.jpg';
+import Mail from './mail.jpg';
 
 class Section extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClick: -1,
+      user: ["Brad Gibson", "011-962-7516", "9278 new road, kilcoole", "brad.gibson@example.com"],
+      default: ["My name is", "My phone number is", "My address is", "My mail address is"],
+      intro: ["Brad Gibson", "My name is"],
+      originalImg: [Name, Phone, Location, Mail],
+      changeImg: [Name_after, Phone_after, Location_after, Mail_after]
+    }
+  }
+
+  makeDefault() {
+   if(this.state.isClick === -1) {
+     return this.state.intro[1];
+   } else if (this.state.isClick === 0) { //name
+     return this.state.default[0];
+   } else if (this.state.isClick === 1) { //phone
+    return this.state.default[1];
+   } else if (this.state.isClick === 2) { //address
+    return this.state.default[2];
+   } else if (this.state.isClick === 3) { //mail
+    return this.state.default[3];
+   } 
+  }
+
+  makeSelect() {
+    if(this.state.isClick === -1) {
+      return this.state.intro[0];
+    } else if (this.state.isClick === 0) { //name
+      return this.state.user[0];
+    } else if (this.state.isClick === 1) { //phone
+     return this.state.user[1];
+    } else if (this.state.isClick === 2) { //address
+     return this.state.user[2];
+    } else if (this.state.isClick === 3) { //mail
+     return this.state.user[3];
+    }
+  }
+
+  makeClick(event) {
+    const clickAlt = event.target.alt; 
+    if(clickAlt === "name") {
+      this.setState({
+        isClick: 0
+      })
+    } else if(clickAlt === "phone") {
+      this.setState({
+        isClick: 1
+      })
+    } else if(clickAlt === "location") {
+      this.setState({
+        isClick: 2
+      })
+    } else if(clickAlt === "mail") {
+      this.setState({
+        isClick: 3
+      })
+    }
+  }
+
   render() {
     return (
       <section>
-        <div className="default_information">{this.props.selectIntro}</div>
-        <div className="select_information">{this.props.selectValue}</div>
-        <ul className="list_information"  onClick={this.props.makeInfo}>
+        <div className="default_information">{this.makeDefault()}</div>
+        <div className="select_information">{this.makeSelect()}</div>
+        <ul className="list_information"  onClick={this.makeClick.bind(this)}>
         <li className="name">
-          <img className="li" src={this.props.data.originalImg[0]} alt="name" />
+          <img 
+            className="li" 
+            src={this.state.isClick === 0 ? this.state.changeImg[0] : this.state.originalImg[0]}
+            alt="name"
+          />
         </li>
         <li className="phone">
-          <img className="li" src={this.props.data.originalImg[1]} alt="phone"/>
+          <img 
+            className="li" 
+            src={this.state.isClick === 1 ? this.state.changeImg[1] : this.state.originalImg[1]} 
+            alt="phone"
+          />
         </li>
         <li className="location">
-          <img className="li" src={this.props.data.originalImg[2]} alt="location"/>
+          <img 
+            className="li" 
+            src={this.state.isClick === 2 ? this.state.changeImg[2] : this.state.originalImg[2]} 
+            alt="location"
+          />
         </li>
         <li className="mail">
-          <img className="li" src={this.props.data.originalImg[3]} alt="mail"/>
+          <img 
+            className="li" 
+            src={this.state.isClick === 3 ? this.state.changeImg[3] : this.state.originalImg[3]}
+            alt="mail"
+          />
         </li>
       </ul>
       </section>
